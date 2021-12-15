f45c8e3a0c255739932dff64f9b952e7d17f22dd
sahil.samantaray <sahil.samantaray@nymble.in>
2021-12-15 11:25:38 +0530

    Added basic endpoints

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+
+const app = express();
+
+app.use(express.json())
+
+
+app.get("/",(req,res) => {
+    res.send("HEllo WOrld")
+})
+
+app.get("/me", (req,res) => {
+    res.send(`Your query ID is ${req.query.id}`)
+} )
+
+app.get("/:id",(req,res) => {
+    const {id} = req.params
+    res.send(`Your ID is ${id}`)
+})
+
+app.post("/me",(req,res) => {
+    const {id,name} = req.body
+    res.send(JSON.stringify({id,name}))
+})
+
+
+app.listen(1337, () => {
+    console.log("Server started at 1337")
+})
